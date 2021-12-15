5a8fc6452d61c3082e3d6d87c2f80f028f5ca5f6
jujku <94424188+jujku@users.noreply.github.com>
2021-12-15 19:04:56 +0800

    Update 未命名1.cpp

--- code/C_code/乘法表/未命名1.cpp
+++ code/C_code/乘法表/未命名1.cpp
@@ -1,18 +1,1 @@
-# include <stdio.h>
-int main (){
-	
-	printf("   1   2   3   4   5   6   7   8   9\n");
-	printf("   -   -   -   -   -   -   -   -   -\n");
-	int p, i, j;
-	for (i = 1; i <= 9; i++){
-		for (p = 10 - i; p < 9; p++ ){
-			printf("    ");
-		}
-		for(j = i; j <= 9; j++){
-			printf("%4d", i * j);
-		}
-		printf("\n");
-	}
-	
-	return 0;
-}
+
